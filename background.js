0109ba7c6be4af6eef449d317feb46a875b7cf60
Abhay Rana <capt.n3m0@gmail.com>
2015-06-17 20:10:32 +0530

    Improves url parsing.
    - Blocks woff2 files
    - More readable code
    - Closes #2

--- background.js
+++ background.js
@@ -1,9 +1,29 @@
+function parseURL(url){
+  var parser = document.createElement('a');
+  parser.href = url;
+  return parser;
+}
+
+function isFontFile(filename){
+  var extensions = [
+    '.woff', '.otf', '.ttf', '.woff2'
+  ];
+
+  for(var i in extensions){
+    var extension = extensions[i];
+    if(filename.slice(-extension.length) == extension){
+      return true;
+    }
+  }
+  return false;
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    var url = details.url.toLowerCase();
-    var cancel = url.slice(-5)=='.woff'||url.slice(-4)=='.otf'||url.slice(-4)=='.ttf';
+    var url = parseURL(details.url.toLowerCase()),
+      filename = url.pathname;
     return {
-      cancel: cancel
+      cancel: isFontFile(filename)
     };
   },
   {
